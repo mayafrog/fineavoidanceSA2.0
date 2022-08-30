67e75a06d2e6a268578e7c5b6c7270c078f71c77
mayafrog <nhan150@hotmail.com>
2022-08-31 00:03:19 +0930

    WIP integrated google maps api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api'
+
+const containerStyle = {
+  width: '60%',
+  height: '1000px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function App() {
   const [cameras, setCameras] = useState([]);
@@ -11,11 +22,43 @@ function App() {
     });
   }, []);
 
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "{PLACEHOLDER}"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  if (!isLoaded) {
+    return
+  }
+
   return (
     <div className="App">
       <header className="App-header">
 
         <img src={logo} className="App-logo" alt="logo" />
+
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          { /* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
 
         {cameras.map((camera, index) => {
           return (

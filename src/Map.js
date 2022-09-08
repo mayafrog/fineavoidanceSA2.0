7df38127de7346e83adac9dbe8bf3abefe35638c
mayafrog <nhan150@hotmail.com>
2022-09-08 16:56:55 +0930

    added display of today's date in Map component

--- src/Map.js
+++ src/Map.js
@@ -3,6 +3,7 @@ import React, {
     useEffect
 } from 'react';
 import { useJsApiLoader, GoogleMap, Marker, InfoWindowF } from '@react-google-maps/api'
+import moment from 'moment';
 
 function Map() {
     const containerStyle = {
@@ -10,10 +11,10 @@ function Map() {
         height: '80vh'
     };
 
-    const center = {
-        lat: -34.921230,
-        lng: 138.599503
-    };
+    // const center = {
+    //     lat: -34.921230,
+    //     lng: 138.599503
+    // };
 
     const options = {
         gestureHandling: 'greedy',
@@ -24,13 +25,16 @@ function Map() {
 
     const [map, setMap] = useState(null);
 
+    const [activeMarker, setActiveMarker] = useState(null);
+
+    const today = moment().format('DD/MM/YYYY');
+
     useEffect(() => {
         fetch('/cameras-today').then(res => res.json()).then(data => {
             setMarkers(data);
         });
     }, []);
 
-    const [activeMarker, setActiveMarker] = useState(null);
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
@@ -46,26 +50,29 @@ function Map() {
     map?.fitBounds(bounds);
 
     return (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            options={options}
-            onLoad={(map) => setMap(map)}
-            onClick={() => setActiveMarker(null)}
-        >
-            {markers[0]?.cameras?.map(({ location, position }) => (
-                <Marker
-                    key={location}
-                    position={position}
-                    onClick={() => setActiveMarker(location)}
-                >
-                    {activeMarker === location ? (
-                        <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
-                            <div style={{ color: "black" }}>{location}</div>
-                        </InfoWindowF>
-                    ) : null}
-                </Marker>
-            ))}
-        </GoogleMap>
+        <div>
+            <p style={{ textAlign: "left", fontSize: 18}}>Today's date: {today}</p>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                options={options}
+                onLoad={(map) => setMap(map)}
+                onClick={() => setActiveMarker(null)}
+            >
+                {markers[0]?.cameras?.map(({ location, position }) => (
+                    <Marker
+                        key={location}
+                        position={position}
+                        onClick={() => setActiveMarker(location)}
+                    >
+                        {activeMarker === location ? (
+                            <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+                                <div style={{ color: "black" }}>{location}</div>
+                            </InfoWindowF>
+                        ) : null}
+                    </Marker>
+                ))}
+            </GoogleMap>
+        </div>
     );
 }
 

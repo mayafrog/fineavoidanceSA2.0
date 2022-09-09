b3733f10c377b1eefc178c2d2a84da2196a7b762
mayafrog <nhan150@hotmail.com>
2022-09-10 07:02:03 +0930

    added mui's dark mode

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import { default as Map } from "./Map.js";
 import { default as List } from "./List.js";
 import { Box, Tab, Typography, Unstable_Grid2 as Grid } from "@mui/material";
 import { TabPanel, TabContext, TabList } from "@mui/lab"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [currentTab, setCurrentTab] = useState('1');
@@ -14,24 +22,23 @@ function App() {
   };
 
   return (
-    <Box className="App">
-      <Box className="App-header">
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
 
-        <Typography variant="h4" component="h1" gutterBottom> Nhan's FineAvoidanceSA2.0 </Typography>
+      <Typography variant="h4" component="h1" gutterBottom textAlign={"center"} marginTop={"20px"}> Nhan's FineAvoidanceSA2.0 </Typography>
 
-        <TabContext value={currentTab}>
-          <Box sx={{ borderBottom: 1, borderColor: 'white' }}>
-            <TabList onChange={handleTab} textColor="inherit" indicatorColor='white'>
-              <Tab label="Map" value="1" />
-              <Tab label="Current Data" value="2" />
-            </TabList>
-          </Box>
-          <TabPanel value="1"> <Map /> </TabPanel>
-          <TabPanel value="2"> <List /> </TabPanel>
-        </TabContext>
+      <TabContext value={currentTab}>
+        <Box sx={{ borderBottom: 1, borderColor: 'white' }}>
+          <TabList onChange={handleTab} textColor="inherit" indicatorColor='white'>
+            <Tab label="Map" value="1" />
+            <Tab label="Current Data" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"> <Map /> </TabPanel>
+        <TabPanel value="2"> <List /> </TabPanel>
+      </TabContext>
 
-      </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 

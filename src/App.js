94650fffa74727c4bde7f22253a77c8493dcdfdb
mayafrog <nhan150@hotmail.com>
2022-09-09 20:26:36 +0930

    moved Map and List components to their own tabs using @mui/lab

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 import { default as Map } from "./Map.js";
 import { default as List } from "./List.js";
-import { Box, Typography } from "@mui/material";
+import { Box, Tab, Typography } from "@mui/material";
+import { TabPanel, TabContext, TabList } from "@mui/lab"
 
 function App() {
+  const [tab, settab] = React.useState('1');
+
+  const handleTab = (event, newtab) => {
+    settab(newtab);
+  };
 
   return (
     <Box className="App">
-      <header className="App-header">
+      <Box className="App-header">
         <Typography variant="h4" component="h1" gutterBottom> Nhan's FineAvoidanceSA2.0 </Typography>
-        <Map />
 
-        <List />
+        <TabContext value={tab}>
+          <Box sx={{ borderBottom: 1, borderColor: 'white' }}>
+            <TabList onChange={handleTab} textColor="white" indicatorColor='white'>
+              <Tab label="Map" value="1" />
+              <Tab label="List" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1"> <Map /> </TabPanel>
+          <TabPanel value="2"> <List /> </TabPanel>
+        </TabContext>
 
-      </header>
-    </Box >
+      </Box>
+    </Box>
   );
 }
 

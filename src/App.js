b8a2c91420c0121a76579c260c5a55dc6db700bc
mayafrog <nhan150@hotmail.com>
2022-09-10 05:13:24 +0930

    small formatting changes

--- src/App.js
+++ src/App.js
@@ -7,22 +7,23 @@ import { Box, Tab, Typography } from "@mui/material";
 import { TabPanel, TabContext, TabList } from "@mui/lab"
 
 function App() {
-  const [tab, settab] = React.useState('1');
+  const [currentTab, setCurrentTab] = useState('1');
 
   const handleTab = (event, newtab) => {
-    settab(newtab);
+    setCurrentTab(newtab);
   };
 
   return (
     <Box className="App">
       <Box className="App-header">
+      
         <Typography variant="h4" component="h1" gutterBottom> Nhan's FineAvoidanceSA2.0 </Typography>
 
-        <TabContext value={tab}>
+        <TabContext value={currentTab}>
           <Box sx={{ borderBottom: 1, borderColor: 'white' }}>
-            <TabList onChange={handleTab} textColor="white" indicatorColor='white'>
+            <TabList onChange={handleTab} textColor="inherit" indicatorColor='white'>
               <Tab label="Map" value="1" />
-              <Tab label="List" value="2" />
+              <Tab label="Current Data" value="2" />
             </TabList>
           </Box>
           <TabPanel value="1"> <Map /> </TabPanel>

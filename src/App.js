df844ef784ef641def0b12abd15a50004cc0ab88
mayafrog <nhan150@hotmail.com>
2022-09-19 07:35:38 +0930

    added colour button, have not added styling for navbar yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { MantineProvider } from '@mantine/core';
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useState } from 'react';
+import { ColourButton } from './components';
 import { Navbar, TabContainer } from './containers';
 
 export default function App() {
+  const [colorScheme, setColorScheme] = useState('dark');
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
   return (
-    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
-      <Navbar></Navbar>
-      <TabContainer></TabContainer>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <Navbar></Navbar>
+        <ColourButton></ColourButton>
+        <TabContainer></TabContainer>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as AccordionList } from "./accordionlist/AccordionList.js";
+export { default as ColourButton } from "./colourbutton/ColourButton.js";
 export { default as DateSelector } from "./dateselector/DateSelector.js";
 export { default as IndividualList } from "./individuallist/IndividualList.js";
 export { default as Map } from "./map/Map.js";

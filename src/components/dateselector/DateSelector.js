5840f2c86b6e48913bdab72d0b8ff77d962bc8b5
mayafrog <nhan150@hotmail.com>
2022-09-18 17:42:12 +0930

    removed unnecessary React.fragment

--- src/components/dateselector/DateSelector.js
+++ src/components/dateselector/DateSelector.js
@@ -1,23 +1,24 @@
 import { DatePicker } from '@mantine/dates';
+import { useMediaQuery } from '@mantine/hooks';
 
 function DateSelector({ selectedDate, setSelectedDate, historicalCameras }) {
     const dayjs = require('dayjs')
+    const isMobile = useMediaQuery('(max-width: 755px)');
 
     return (
-        <>
-            <DatePicker
-                label="Selected date"
-                value={dayjs(selectedDate).toDate()}
-                onChange={(newDate) => {
-                    setSelectedDate(newDate);
-                }}
-                clearable={false}
-                inputFormat="DD/MM/YYYY"
-                minDate={dayjs(historicalCameras[0]?.date, 'DD/MM/YYYY').toDate()}
-                maxDate={dayjs(historicalCameras[historicalCameras.length - 1]?.date, 'DD/MM/YYYY').toDate()}
-                withAsterisk
-            />
-        </>
+        <DatePicker
+            label="Selected date"
+            value={dayjs(selectedDate).toDate()}
+            onChange={(newDate) => {
+                setSelectedDate(newDate);
+            }}
+            clearable={false}
+            inputFormat="DD/MM/YYYY"
+            minDate={dayjs(historicalCameras[0]?.date, 'DD/MM/YYYY').toDate()}
+            maxDate={dayjs(historicalCameras[historicalCameras.length - 1]?.date, 'DD/MM/YYYY').toDate()}
+            withAsterisk
+            dropdownType={isMobile ? 'modal' : 'popover'}
+        />
     )
 }
 

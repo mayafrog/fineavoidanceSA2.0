fd1b31b78ccf966ecb212bd252b4d9b52d4c90c2
mayafrog <nhan150@hotmail.com>
2022-09-19 12:03:14 +0930

    simplified colour switch

--- src/components/colourbutton/ColourButton.js
+++ src/components/colourbutton/ColourButton.js
@@ -1,53 +1,35 @@
-import { Center, createStyles, Group, Text, UnstyledButton, useMantineColorScheme } from '@mantine/core';
-import { upperFirst } from '@mantine/hooks';
+import { Box, Center, Group, SegmentedControl, useMantineColorScheme } from '@mantine/core';
 import { IconMoonStars, IconSun } from '@tabler/icons';
 
-const useStyles = createStyles((theme) => ({
-  control: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderRadius: 1000,
-    paddingLeft: theme.spacing.sm,
-    paddingRight: 4,
-    width: 136,
-    height: 36,
-  },
-
-  iconWrapper: {
-    height: 28,
-    width: 28,
-    borderRadius: 28,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.dark[4],
-    color: theme.colorScheme === 'dark' ? theme.black : theme.colors.blue[2],
-  },
-
-  value: {
-    lineHeight: 1,
-  },
-}));
-
 function ColourButton() {
-  const { classes } = useStyles();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const Icon = colorScheme === 'dark' ? IconSun : IconMoonStars;
 
   return (
     <Group position="center">
-      <UnstyledButton
-        aria-label="Toggle theme"
-        className={classes.control}
-        onClick={() => toggleColorScheme()}
-      >
-        <Text size="sm" className={classes.value}>
-          {upperFirst(colorScheme === 'light' ? 'dark' : 'light')} theme
-        </Text>
-
-        <Center className={classes.iconWrapper}>
-          <Icon size={18} stroke={1.5} />
-        </Center>
-      </UnstyledButton>
+      <SegmentedControl
+        value={colorScheme}
+        onChange={(value) => toggleColorScheme(value)}
+        data={[
+          {
+            value: 'light',
+            label: (
+              <Center>
+                <IconSun size={16} stroke={1.5} />
+                <Box ml={10}>Light</Box>
+              </Center>
+            ),
+          },
+          {
+            value: 'dark',
+            label: (
+              <Center>
+                <IconMoonStars size={16} stroke={1.5} />
+                <Box ml={10}>Dark</Box>
+              </Center>
+            ),
+          },
+        ]}
+      />
     </Group>
   );
 }
